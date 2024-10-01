504e2d7af02658f42a7d575aa006f7ad233ac393
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 20:15:58 +0200

    addressDTO was developed

--- src/main/java/dev/aitor/pc_builder/address/AddressDTO.java
+++ src/main/java/dev/aitor/pc_builder/address/AddressDTO.java
@@ -1,5 +1,19 @@
 package dev.aitor.pc_builder.address;
 
+import dev.aitor.pc_builder.profile.ProfileDTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressDTO {
-    
+
+    private long id;
+    private String street;
+    private String city;
+    private String region;
+    private String postalCode;
+    private String country;
+    private ProfileDTO profile;
 }
