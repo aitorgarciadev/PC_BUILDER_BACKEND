16e696e8eb29d7c4b8c3cfa8d96f295aaaf2be72
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 23:48:59 +0200

    cart dto developed

--- src/main/java/dev/aitor/pc_builder/cart/Cart.java
+++ src/main/java/dev/aitor/pc_builder/cart/Cart.java
@@ -1,5 +1,30 @@
 package dev.aitor.pc_builder.cart;
 
+import java.util.List;
+
+import dev.aitor.pc_builder.cart_item.CartItem;
+import dev.aitor.pc_builder.user.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private long id;
     
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cart_items;
+
+    private float totalPrice;
 }
--- src/main/java/dev/aitor/pc_builder/cart/CartDTO.java
+++ src/main/java/dev/aitor/pc_builder/cart/CartDTO.java
@@ -1,5 +1,19 @@
 package dev.aitor.pc_builder.cart;
 
+import java.util.List;
+
+import dev.aitor.pc_builder.cart_item.CartItemDTO;
+import dev.aitor.pc_builder.user.UserDTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartDTO {
     
+    private Long id;
+    private UserDTO user;
+    private List<CartItemDTO> cartItems;
+    private float totalPrice;
 }
