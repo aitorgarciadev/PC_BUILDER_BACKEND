4e6b77a4236f0cda736813fcd0f1e6332fb709ac
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 23:44:14 +0200

    accoount settings repository developed

---
+++ src/main/java/dev/aitor/pc_builder/account_settings/AccountSettingsRepository.java
@@ -0,0 +1,7 @@
+package dev.aitor.pc_builder.account_settings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountSettingsRepository extends JpaRepository<AccountSettings, Long> {
+    
+}
