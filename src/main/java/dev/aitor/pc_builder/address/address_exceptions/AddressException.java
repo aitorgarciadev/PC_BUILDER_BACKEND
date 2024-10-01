faa65aa69e0f4ef1c98ec2edfda94a9e7ac0de71
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 23:39:15 +0200

    address excepctions developed and service fully developed

---
+++ src/main/java/dev/aitor/pc_builder/address/address_exceptions/AddressException.java
@@ -0,0 +1,8 @@
+package dev.aitor.pc_builder.address.address_exceptions;
+
+public class AddressException extends RuntimeException {
+    public AddressException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/dev/aitor/pc_builder/address/address_exceptions/AddressNotFoundException.java
@@ -0,0 +1,9 @@
+package dev.aitor.pc_builder.address.address_exceptions;
+
+public class AddressNotFoundException extends AddressException {
+    public AddressNotFoundException(String message) {
+        super(message);
+    }
+
+}
+
