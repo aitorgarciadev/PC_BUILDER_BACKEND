8bed1fd3e42dd5722777198e7e067cd72fb222d1
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 23:13:49 +0200

    user exceptions were developed

--- src/main/java/dev/aitor/pc_builder/user/user_exceptions/UserNotFoundException.java
+++ src/main/java/dev/aitor/pc_builder/user/user_exceptions/UserNotFoundException.java
@@ -1,5 +1,12 @@
 package dev.aitor.pc_builder.user.user_exceptions;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends UserException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
     
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
