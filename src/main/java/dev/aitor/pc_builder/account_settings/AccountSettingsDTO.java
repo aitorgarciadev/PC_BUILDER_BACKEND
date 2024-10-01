5526a259ff1ef0d929f64d1620085aa096f9be4c
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 23:42:57 +0200

    account settins DTO developed

--- src/main/java/dev/aitor/pc_builder/account_settings/AccountSettingsDTO.java
+++ src/main/java/dev/aitor/pc_builder/account_settings/AccountSettingsDTO.java
@@ -1,5 +1,15 @@
 package dev.aitor.pc_builder.account_settings;
 
+import dev.aitor.pc_builder.user.UserDTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountSettingsDTO {
     
+    private Long id;
+    private String language;
+    private UserDTO user;
 }
