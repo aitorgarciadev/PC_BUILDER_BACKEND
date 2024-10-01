299aa12d9720172580ba9cbfc78cf2460127d4bc
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 20:18:40 +0200

    user repository was developed

--- src/main/java/dev/aitor/pc_builder/user/UserRepository.java
+++ src/main/java/dev/aitor/pc_builder/user/UserRepository.java
@@ -1,5 +1,12 @@
 package dev.aitor.pc_builder.user;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
