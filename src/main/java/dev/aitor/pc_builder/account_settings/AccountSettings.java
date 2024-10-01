5d12f6fd5c3764200d164e111e3437d7283099fa
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-01 23:41:46 +0200

    account settings developed

--- src/main/java/dev/aitor/pc_builder/account_settings/AccountSettings.java
+++ src/main/java/dev/aitor/pc_builder/account_settings/AccountSettings.java
@@ -1,5 +1,27 @@
 package dev.aitor.pc_builder.account_settings;
 
+import dev.aitor.pc_builder.user.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account_settings")
 public class AccountSettings {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_settings_id")
+    private long id;
+
+    private String language; // en, es
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
 }
