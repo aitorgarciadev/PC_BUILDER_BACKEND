855cdd532453fa8ae6c12e71124d2ea7e239f070
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-02 19:13:10 +0200

    review dto developed

--- src/main/java/dev/aitor/pc_builder/review/ReviewDTO.java
+++ src/main/java/dev/aitor/pc_builder/review/ReviewDTO.java
@@ -1,5 +1,17 @@
 package dev.aitor.pc_builder.review;
 
+import dev.aitor.pc_builder.order_items.OrderItemDTO;
+import dev.aitor.pc_builder.user.UserDTO;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewDTO {
     
+    private Long id;
+    private int rating;
+    private OrderItemDTO orderItem;
+    private UserDTO user;
 }
