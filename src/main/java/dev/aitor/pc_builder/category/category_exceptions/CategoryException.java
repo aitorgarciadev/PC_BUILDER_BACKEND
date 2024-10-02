ed4e813c0a38abd966643231f0845a32f4e6adf7
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-02 17:54:41 +0200

    category exception

--- src/main/java/dev/aitor/pc_builder/category/category_exceptions/CategoryException.java
+++ src/main/java/dev/aitor/pc_builder/category/category_exceptions/CategoryException.java
@@ -1,5 +1,8 @@
 package dev.aitor.pc_builder.category.category_exceptions;
 
-public class CategoryException {
-    
+public class CategoryException extends RuntimeException {
+    public CategoryException(String message) {
+        super(message);
+    }
+
 }
