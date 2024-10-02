4782777cc4f50b5690740dabaf1a3ff030fd74ae
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-02 17:32:36 +0200

    product exceptions done

---
+++ src/main/java/dev/aitor/pc_builder/product/product_exceptions/ProductException.java
@@ -0,0 +1,9 @@
+package dev.aitor.pc_builder.product.product_exceptions;
+
+public class ProductException extends RuntimeException {
+    public ProductException(String message) {
+        super(message);
+    }
+
+
+}
---
+++ src/main/java/dev/aitor/pc_builder/product/product_exceptions/ProductNotFoundException.java
@@ -0,0 +1,10 @@
+package dev.aitor.pc_builder.product.product_exceptions;
+
+
+public class ProductNotFoundException extends ProductException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
