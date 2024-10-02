1713aedf0c7e7ef803f5c949b84ee14df1a88a34
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-02 17:55:38 +0200

    too many categories and categories service fully developed

--- src/main/java/dev/aitor/pc_builder/category/category_exceptions/TooManyCategoriesSelectedException.java
+++ src/main/java/dev/aitor/pc_builder/category/category_exceptions/TooManyCategoriesSelectedException.java
@@ -1,5 +1,7 @@
 package dev.aitor.pc_builder.category.category_exceptions;
 
-public class TooManyCategoriesSelectedException {
-    
+public class TooManyCategoriesSelectedException extends RuntimeException {
+    public TooManyCategoriesSelectedException(String message) {
+        super(message);
+    }
 }
