c939552e4c17bd35dd66591bd1748657a8b218d8
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-10-02 19:10:36 +0200

    review updated

--- src/main/java/dev/aitor/pc_builder/review/Review.java
+++ src/main/java/dev/aitor/pc_builder/review/Review.java
@@ -1,5 +1,31 @@
 package dev.aitor.pc_builder.review;
 
+import dev.aitor.pc_builder.order_items.OrderItem;
+import dev.aitor.pc_builder.user.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reviews")
 public class Review {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+    private int rating;
+
+    @OneToOne
+    @JoinColumn(name = "order_item_id", referencedColumnName = "order_item_id")
+    private OrderItem orderItem;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
 }
